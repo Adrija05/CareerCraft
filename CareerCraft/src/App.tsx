6ee2852bf77264c893d20f95bc67f49e00a2a9b2
Prajusha2004 <146667630+Prajusha2004@users.noreply.github.com>
2025-04-14 01:00:07 +0530

    Router setup done, Changes made and Started with Header and footer a bit .

--- CareerCraft/src/App.tsx
+++ CareerCraft/src/App.tsx
@@ -1,10 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PublicLayout } from "@/layouts/publiclayout";
+import HomePage from "@/routes/home";
+import AuthenticationLayout from "@/layouts/auth-layout";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up";
+import ProtectedRoutes from "./layouts/protected-routes";
+import { MainLayout } from "@/layouts/main-layout";
 
 const App = () => {
   return (
-    <div className="text-blue-500">
-      App
-    </div>
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/*authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin/*" element={<SignInPage />} />
+          <Route path="/signup/*" element={<SignUpPage />} />
+        </Route>
+
+        {/* Protected routes can go here */}
+
+        <Route element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>}>
+        {/* add all the protect routes */}
+        
+        
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
