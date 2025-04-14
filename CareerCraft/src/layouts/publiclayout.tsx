085a60dc0668d4c33a08a0263e87d8916f0cbfa6
Adrija Sengupta <137429634+Adrija05@users.noreply.github.com>
2025-04-15 02:17:18 +0530

    changes: home screen completed

--- CareerCraft/src/layouts/publiclayout.tsx
+++ CareerCraft/src/layouts/publiclayout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
-import Footer from "@/components/footer";
 import AuthHanlder from "@/hanlders/auth-handler";
+import { Footer } from "@/components/footer";
 
 export const PublicLayout = () => {
   return (
