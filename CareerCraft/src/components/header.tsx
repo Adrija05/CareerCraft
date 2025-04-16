b2864dd36a57c1f1bf1bfdc8a9d4c31ff6e69c05
Prajusha2004 <146667630+Prajusha2004@users.noreply.github.com>
2025-04-16 16:04:42 +0530

    Start page and Feedback done

--- CareerCraft/src/components/header.tsx
+++ CareerCraft/src/components/header.tsx
@@ -1,53 +1,53 @@
-import { useAuth } from "@clerk/clerk-react";
 import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
 import { Container } from "./container";
 import { LogoContainer } from "./logo-container";
 import { NavigationRoutes } from "./navigation-routes";
 import { NavLink } from "react-router-dom";
 import { ProfileContainer } from "./profile-container";
-import { ToggleContainer }  from "./toggle-container";
+import { ToggleContainer } from "./toggle-container";
 
-const header = () => {
-
+const Header = () => {
   const { userId } = useAuth();
 
   return (
-    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+    <header
+      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    >
       <Container>
         <div className="flex items-center gap-4 w-full">
-           {/* logo section */}
-           <LogoContainer />
+          {/* logo section */}
+          <LogoContainer />
 
-           {/* navigation section */}
-           <nav>
-           <NavigationRoutes />
-           {userId && (
+          {/* navigation section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes />
+            {userId && (
               <NavLink
                 to={"/generate"}
                 className={({ isActive }) =>
                   cn(
-                    "flex text-base text-neutral-600",
-                    isActive && "flex text-neutral-900 font-semibold"
+                    "text-base text-neutral-600",
+                    isActive && "text-neutral-900 font-semibold"
                   )
                 }
               >
                 Take An Interview
               </NavLink>
             )}
-           </nav>
-           <div className="ml-auto flex items-center gap-6">
+          </nav>
+
+          <div className="ml-auto flex items-center gap-6">
             {/* profile section */}
             <ProfileContainer />
 
             {/* mobile toggle section */}
             <ToggleContainer />
-
-            </div>
-
+          </div>
         </div>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default Header;
